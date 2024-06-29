72aa6b22f42856f52daf3196f8521bf77e58a2eb
Frooge <jaantaros@gmail.com>
2024-06-29 22:52:35 +0800

    ui, particle, and player collision

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import Phaser from 'phaser'
+import WebFont from 'webfontloader'
 
 const sizes = {
   width: 960,
@@ -9,13 +10,30 @@ const sizes = {
 class GameScene extends Phaser.Scene{
   constructor(){
     super("scene-game")
+    this.start = false
+    this.startButton
+    this.introText
+    this.tutorialText
+    this.countDownText
+    this.countDownTime = 3
+    this.countDownStart = 0
+    this.scrollSpeed = .25
+    this.scoreText
+    this.score = 0
+    this.scoreLastIncrement = 0
+    this.scoreBuffer = 100
+    this.playerHPUI
     this.player
     this.cursors
     this.keys
+    this.playerHP = 3
+    this.playerHPIcons
+    this.playerLastHit = 0
+    this.playerIFrames = 1000
     this.playerSpeed = 500
     this.playerLasers
-    this.playerLaserSpeed = 20
-    this.playerLaserCooldown = 250
+    this.playerLaserSpeed = 12
+    this.playerLaserCooldown = 300
     this.playerLaserLastFired = 0 // Timestamp of the last fired laser
     this.meteors
     this.meteorCooldown = 2500
@@ -27,15 +45,17 @@ class GameScene extends Phaser.Scene{
     this.load.image("bg", "/assets/Backgrounds/purple.png")
     this.load.image("player", "/assets/playerShip1_orange.png")
     this.load.image("playerLaser", "/assets/Lasers/laserBlue01.png")
+    this.load.image("playerLaserParticle", "/assets/Lasers/laserBlue08.png")
     this.load.image("bigMeteor1", "/assets/Meteors/meteorBrown_big1.png")
     this.load.image("bigMeteor2", "/assets/Meteors/meteorBrown_big2.png")
     this.load.image("bigMeteor3", "/assets/Meteors/meteorBrown_big3.png")
     this.load.image("bigMeteor4", "/assets/Meteors/meteorBrown_big4.png")
+    this.load.image("playerHPIcon", "/assets/UI/playerLife1_orange.png")
 
     this.cursors = this.input.keyboard.createCursorKeys()
     this.keys = this.input.keyboard.addKeys('W,A,S,D,SPACE')
 
-    this.boundsCollisionCategory = this.matter.world.nextCategory();
+    this.boundsCollisionCategory = 1;
     this.playerCollisionCategory = this.matter.world.nextCategory();
     this.playerLaserCollisionCategory = this.matter.world.nextCategory();
     this.enemyCollisionCategory = this.matter.world.nextCategory();
@@ -46,13 +66,14 @@ class GameScene extends Phaser.Scene{
 
     this.bg = this.add.tileSprite(0,0,sizes.width,sizes.height,"bg").setOrigin(0,0)
 
-    // Create a Matter.js collision category for world bounds
-
-    this.player = this.matter.add.image(sizes.width-(sizes.width-100),(sizes.height/2)-40, "player")
-    this.player.angle = 90
-    this.player.setFixedRotation() // Prevent rotation when colliding
-    this.player.setCollisionCategory(this.playerCollisionCategory) // Set player to collide with world bounds
+    this.spawnIntroText();
+    this.spawnTutorialText();
+    this.spawnCountDownText();
+    this.spawnScoreText();
     
+
+    // Create a Matter.js collision category for world bounds
+    this.spawnPlayer();
 
     this.playerLasers = this.add.group();
     this.meteors = this.add.group();
@@ -60,30 +81,68 @@ class GameScene extends Phaser.Scene{
     // Create world bounds
     const { width, height } = sizes
     const thickness = 64 // Thickness of the bounds
-    this.matter.world.setBounds(0, 0, width, height, thickness, true, true, true, true, { collisionFilter: { category: this.boundsCollisionCategory } })
+    this.matter.world.setBounds(0, 0, width, height, thickness, true, true, true, true)
 
     this.matter.world.on('collisionstart', (event) => {
       event.pairs.forEach(pair => {
         const bodyA = pair.bodyA;
         const bodyB = pair.bodyB;
 
+      // Check if collision involves player and enemy
+        if ((bodyA.collisionFilter.category === this.playerCollisionCategory && bodyB.collisionFilter.category === this.enemyCollisionCategory) ||
+          (bodyA.collisionFilter.category === this.enemyCollisionCategory && bodyB.collisionFilter.category === this.playerCollisionCategory)) {
+        
+          const player = bodyA.collisionFilter.category === this.playerCollisionCategory ? bodyA : bodyB;
+          const enemy = bodyA.collisionFilter.category === this.enemyCollisionCategory ? bodyA : bodyB;
+          
+          
+          const time = this.time.now // get current time
 
-        // Check if collision involves playerLaser and meteor
+          if (time > this.playerLastHit + this.playerIFrames) {
+            this.reducePlayerHP()
+            this.playerLastHit = time
+          }
+
+        }
+        
+        // Check if collision involves playerLaser and enemy
         if ((bodyA.collisionFilter.category === this.playerLaserCollisionCategory && bodyB.collisionFilter.category === this.enemyCollisionCategory) ||
           (bodyA.collisionFilter.category === this.enemyCollisionCategory && bodyB.collisionFilter.category === this.playerLaserCollisionCategory)) {
         
           const laser = bodyA.collisionFilter.category === this.playerLaserCollisionCategory ? bodyA : bodyB;
-          const meteor = bodyA.collisionFilter.category === this.enemyCollisionCategory ? bodyA : bodyB;
+          const enemy = bodyA.collisionFilter.category === this.enemyCollisionCategory ? bodyA : bodyB;
 
-          this.damageEntity(meteor.gameObject, laser.gameObject);
+          this.damageEntity(enemy.gameObject, laser.gameObject);
+          let laserParticle = laser.gameObject.laserParticle
+          laserParticle.visible = true
+          this.time.addEvent({
+              delay: 100, // Delay in milliseconds (1000 ms = 1 second)
+              callback: function() {laserParticle.destroy()},
+              callbackScope: this,
+              loop: laserParticle
+          });
           laser.gameObject.destroy()
       }
       });
     });
+
+    WebFont.load({
+      google: {
+        families: ["Press Start 2P"]
+      },
+      active: () => {
+        this.introText.setFontFamily('"Press Start 2P"')
+        this.tutorialText.setFontFamily('"Press Start 2P"')
+        this.countDownText.setFontFamily('"Press Start 2P"')
+        this.scoreText.setFontFamily('"Press Start 2P"')
+      }
+    })
   }
 
   update (time, delta)
   {
+    
+
     this.playerLasers.getChildren().forEach(laser => {
       // Check if laser is outside the world bounds
       if (this.isOutsideWorldBoundsRight(laser)) {
@@ -97,9 +156,14 @@ class GameScene extends Phaser.Scene{
       }
     });
 
-    const scrollSpeed = 1;
-    
-    this.bg.tilePositionX += scrollSpeed;
+    this.playerLasers.getChildren().forEach(laser => {
+      if (laser.laserParticle) {
+          laser.laserParticle.x = laser.x+20;
+          laser.laserParticle.y = laser.y;
+      }
+    });
+
+    this.bg.tilePositionX += this.scrollSpeed;
 
     const {left, right, up, down} = this.cursors
     const { W, A, S, D, SPACE } = this.keys
@@ -140,7 +204,90 @@ class GameScene extends Phaser.Scene{
       this.fireLaser(time);
     }
 
+    if (this.start) {
+      this.gameStart(time)
+    } else if(this.countDownText && (SPACE.isDown && this.countDownText.visible == false)) {
+      this.introText.destroy()
+      this.tutorialText.destroy()
+      this.countDownText.visible = true
+      this.countDown = this.time.delayedCall()
+      this.time.addEvent({
+          delay: 1000, // Delay in milliseconds (1000 ms = 1 second)
+          callback: this.countDownNow,
+          callbackScope: this,
+          loop: this.countDownText
+      });
+    }
+
+    if(this.countDownText && (this.countDownText.visible && this.countDownTime >= -1)) {
+      if(this.countDownTime == 0) {
+        this.countDownText.x = sizes.width/2 - 50
+        this.countDownText.setText('GO!')
+      } else if(this.countDownTime == -1) {
+        this.countDownText.destroy()
+        this.createPlayerHP();
+        this.scoreText.visible = true
+        this.start = true
+      } else {
+        this.countDownText.setText(`${this.countDownTime}`)
+      }
+    }
+  }
+
+  gameStart(time) {
+    // level 1
+    this.accelerateScroll(2)
+    this.generatePoints(time)
     this.createMeteor(time)
+  }
+
+  accelerateScroll(limit) {
+    if(this.scrollSpeed <= limit) {
+      this.scrollSpeed += 0.01
+    }
+  }
+
+  generatePoints(time) {
+    if (time > this.scoreLastIncrement + this.scoreBuffer) {
+      this.score += Math.floor(this.scrollSpeed)
+      this.scoreText.setText(this.score.toString().padStart(6,'0'))
+      this.scoreLastIncrement = time
+    }
+    
+  }
+
+  countDownNow() {
+    this.countDownTime -= 1
+  }
+
+  spawnScoreText() {
+    const scoreText = this.add.text(sizes.width - 200, 30, "000000", {
+      font: "25px"
+    })
+    scoreText.visible = false
+    this.scoreText = scoreText
+  }
+
+  spawnIntroText() {
+    const introText = this.add.text(sizes.width/2 - 240, 30, "Welcome to Space War", {
+      font: "25px"
+    })
+    this.introText = introText
+  }
+
+  spawnTutorialText() {
+    const tutorialText = this.add.text(sizes.width/2 - 400, sizes.height - 50, "Use WASD to move and SPACE to fire", {
+      font: "25px"
+    })
+    this.tutorialText = tutorialText
+  }
+
+  spawnCountDownText() {
+    const countDownText = this.add.text(sizes.width/2 - 25, sizes.height/2 - 50, "3", {
+      font: "64px"
+    })
+    countDownText.visible = false
+    this.countDownText = countDownText
   }
 
   isOutsideWorldBoundsLeft(object) {
@@ -180,6 +327,40 @@ class GameScene extends Phaser.Scene{
     }
   }
 
+  createPlayerHP() {
+    const widthDiff = 50
+    const playerHPArray = Array.from({ length: this.playerHP }, (_, x) => x);
+
+    const playerHPIcons = playerHPArray.map((x) => {
+      return this.add.image(20 + widthDiff * (x+1), 40,"playerHPIcon")
+    })
+
+    this.playerHPIcons = playerHPIcons
+  }
+
+  reducePlayerHP() {
+    if(this.playerHPIcons === undefined || this.playerHPIcons.length === 0) {
+      this.gameOver()
+    }
+
+    let lastIndex = this.playerHPIcons.length - 1
+
+    this.playerHPIcons[lastIndex].destroy()
+    this.playerHPIcons.pop()
+  }
+
+  spawnPlayer() {
+    const player = this.matter.add.image(sizes.width-(sizes.width-100),(sizes.height/2)-40, "player")
+    player.angle = 90
+    player.setFixedRotation() // Prevent rotation when colliding
+    player.setFriction(0);
+    player.setFrictionAir(0);
+    player.setCollisionCategory(this.playerCollisionCategory)
+    player.setCollidesWith([this.enemyCollisionCategory, this.boundsCollisionCategory])
+
+    this.player = player
+  }
+
   spawnLaser() {
     const laser = this.matter.add.image(this.player.x, this.player.y, 'playerLaser');
     laser.angle = 90;
@@ -190,6 +371,10 @@ class GameScene extends Phaser.Scene{
     laser.setCollidesWith([this.enemyCollisionCategory])
 
     laser.dmg = 1
+
+    laser.laserParticle = this.add.image(laser.x+20, laser.y, 'playerLaserParticle');
+    laser.laserParticle.visible = false
+    
 
     this.playerLasers.add(laser)
   }
@@ -208,9 +393,10 @@ class GameScene extends Phaser.Scene{
     meteor.setFriction(0);
     meteor.setFrictionAir(0);
     meteor.setCollisionCategory(this.enemyCollisionCategory)
-    meteor.setCollidesWith([this.enemyCollisionCategory, this.playerCollisionCategory, this.playerLaserCollisionCategory, this.boundsCollisionCategory])
+    meteor.setCollidesWith([this.enemyCollisionCategory, this.playerCollisionCategory, this.playerLaserCollisionCategory])
 
     meteor.hp = 3
+    meteor.points = 64
 
     this.meteors.add(meteor)
   }
@@ -219,8 +405,25 @@ class GameScene extends Phaser.Scene{
     entity.hp -= attack.dmg;
 
     if(entity.hp <= 0) {
-      entity.destroy();
+      this.destroyEntity(entity)
     }
+  }
+
+  damagePlayer() {
+    this.playerHP -= 1
+
+    if(this.playerHP <= 0) {
+      this.gameOver()
+    }
+  }
+
+  destroyEntity(entity) {
+    this.score += entity.points
+    entity.destroy()
+  }
+
+  gameOver() {
+    console.log('YOU DIEEDD!!!!!')
   }
 }
 
@@ -233,7 +436,7 @@ var config = {
     default: 'matter',
     matter:{
       gravity: {y: 0},
-      debug: true
+      debug: false
     }
   },
   scene: [GameScene]
